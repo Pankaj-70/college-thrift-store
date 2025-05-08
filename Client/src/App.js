ddd8f2c0b4dfcf626347c9d956372feb5e09664e
Pankaj Sharma <grgnld3970@gmail.com>
2025-05-08 09:18:12 +0530

    code

---
+++ Client/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import ProductPage from "./components/ProductPage";
+import UserProfile from "./components/UserProfile";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+import LandingPage from "./components/Landing";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/add-product" element={<AddProduct />} />
+            <Route path="/product/:id/edit" element={<EditProduct />} />
+            <Route path="/profile/:id" element={<UserProfile />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
